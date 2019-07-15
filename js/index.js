c92354352d765c60f0a6a61b0968d5b28f883e79
Panacci Rocco <panaccir@nj-00118-001.mediastudies.ca>
2019-07-15 10:44:23 -0400

    Update instructions

--- js/index.js
+++ js/index.js
@@ -93,8 +93,8 @@ const App = () => {
     {r: 255, g: 255, b: 0},
   ];
 
-  // 1. Test adding a second palette Array called palette2, modify the App's
-  //    return to create a second <Palette />
+  // 1. Test adding a second palette Array called `palette2`, modify the `<App />`
+  //    return to create a second <Palette /> that uses the values from `palette2`
   // 2. Modify the CSS so that the `grid-template-columns` property of `.app` 
   //    matches the number of pallets you have
   // 3. Create an array called `palettes` that holds the arrays `palette1` and `palette2`
